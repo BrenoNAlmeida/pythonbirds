8bad45fba96afebef289edf975014ef08377bff8
Breno Nascimento de Almeida <brenonascialmeida@gmail.com>
2020-11-02 02:00:56 -0300

    Update pessoa.py

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,8 +1,10 @@
 class Pessoa:
+    olhos = 2 # atributo de classe padrão no instanciamento de todos os objetos da classe
+
     def __init__(self,*filhos, nome = None, idade = 35):#constrtur de uma classe
         self.idade = idade
         self.nome = nome
-        self.filhos = list(filhos)
+        self.filhos = list(filhos) # atributo complexo
     def cumprimentar(self):
         return 'ola'
 if __name__ == '__main__':
@@ -14,3 +16,6 @@ if __name__ == '__main__':
     print(bia.filhos)
     for filho in bia.filhos:
         print(filho.nome)
+    breno.sobrenome = "almeida" # instanciando um atributo dinamico 
+    del bia.filhos #remoção de atributo de maneira dinamica 
+    breno.__dict__ #visualização dos atributos presentes no objeto
